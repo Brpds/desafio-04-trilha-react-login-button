bd19b466fb279ce55501dc46be11e6c46a526167
Brunno Roberto <brunnophoenix@gmail.com>
2024-10-23 00:46:07 -0300

    Resposta ao desafio 04 Bootcamp DIO e XP Inc, Validação de login com typescript

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,17 +1,20 @@
 import styled from 'styled-components';
+import { IButtonProps } from './types';
 
 
-export const ButtonContainer = styled.button`
+export const ButtonContainer = styled.button<IButtonProps>`
     width: 100%;
     height: 42px;
-    background-color: #81259D;
+    background-color: ${({disabled}) => disabled ? '#ccc' : '#81259D'};
     color: #FFF;
+    cursor: ${({disabled}) => disabled ? 'auto' : 'auto'};
+    opacity: ${({disabled}) => disabled ? '0.6' : '1'};
 
-    border: 1px solid #81259D;
+    border: ${({disabled}) => disabled ? '0' : '1px solid #81259D'};
     border-radius: 21px;
 
     &:hover {
         opacity: 0.6;
-        cursor:pointer;
+        cursor: ${({disabled}) => disabled ? 'auto' : 'pointer'};
     }
 `
